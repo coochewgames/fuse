d708484944063f5a792aada98ee84d9961475755
Roddy McNeill <rd.mcn@ccgapps.net.au>
2025-03-02 15:41:19 +1100

    Fix up logging to work correctly with logging levels; finally remove the original Perl source generation file.

--- logging.c
+++ logging.c
@@ -4,24 +4,48 @@
 #include "logging.h"
 
 
-static LOG_LEVEL log_level = INFO;
+static LOG_LEVEL min_level_to_log = INFO;
+
+static const char *get_log_level_name(LOG_LEVEL level);
+
 
 void set_log_level(LOG_LEVEL level) {
-    log_level = level;
+    min_level_to_log = level;
 }
 
-void log_message(const char *filename, unsigned int line, const char *function, const char *level, const char *message, ...) {
+void log_message(const char *filename, unsigned int line, const char *function, LOG_LEVEL level, const char *message, ...) {
+    if (level < min_level_to_log) {
+        return;
+    }
+
     va_list args;
     va_start(args, message);
 
-    if (strcmp(level, "DEBUG") == 0) {
-        fprintf(stderr, "[%s] %s() - ", level, function);
+    if (level == DEBUG) {
+        fprintf(stderr, "[%s] %s() - ", get_log_level_name(level), function);
     } else {
-        fprintf(stderr, "[%s] %s:%d - ", level, filename, line);
+        fprintf(stderr, "[%s] %s:%d - ", get_log_level_name(level), filename, line);
     }
 
     vfprintf(stderr, message, args);
     fprintf(stderr, "\n");
 
     va_end(args);
+}
+
+static const char *get_log_level_name(LOG_LEVEL level) {
+    switch (level) {
+        case DEBUG:
+            return "DEBUG";
+        case INFO:
+            return "INFO";
+        case WARNING:
+            return "WARNING";
+        case ERROR:
+            return "ERROR";
+        case FATAL:
+            return "FATAL";
+        default:
+            return "UNKNOWN";
+    }
 }
--- logging.h
+++ logging.h
@@ -11,14 +11,14 @@ typedef enum {
     FATAL
 } LOG_LEVEL;
 
-#define DEBUG(...) log_message(__FILE__, __LINE__, __func__, "DEBUG", ##__VA_ARGS__)
-#define INFO(...) log_message(__FILE__, __LINE__, __func__, "INFO", ##__VA_ARGS__)
-#define WARNING(...) log_message(__FILE__, __LINE__, __func__, "WARNING", ##__VA_ARGS__)
-#define ERROR(...) log_message(__FILE__, __LINE__, __func__, "ERROR", ##__VA_ARGS__)
-#define FATAL(...) log_message(__FILE__, __LINE__, __func__, "FATAL", ##__VA_ARGS__)
+#define DEBUG(...) log_message(__FILE__, __LINE__, __func__, DEBUG, ##__VA_ARGS__)
+#define INFO(...) log_message(__FILE__, __LINE__, __func__, INFO, ##__VA_ARGS__)
+#define WARNING(...) log_message(__FILE__, __LINE__, __func__, WARNING, ##__VA_ARGS__)
+#define ERROR(...) log_message(__FILE__, __LINE__, __func__, ERROR, ##__VA_ARGS__)
+#define FATAL(...) log_message(__FILE__, __LINE__, __func__, FATAL, ##__VA_ARGS__)
 
 
 void set_log_level(LOG_LEVEL level);
-void log_message(const char *filename, unsigned int line, const char *function, const char *level, const char *message, ...);
+void log_message(const char *filename, unsigned int line, const char *function, LOG_LEVEL level, const char *message, ...);
 
 #endif
